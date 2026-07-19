649e06e376050645c9cbecc049b9f1b94cb25e65
Ibrahim-Nasser0 <176777704+Ibrahim-Nasser0@users.noreply.github.com>
2026-07-19 12:14:13 +0300

    refactor: remove i18n configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const i18nConfig = require('./src/config/next-i18next.config.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
-  i18n: i18nConfig.i18n,
 };
 
 export default nextConfig;
